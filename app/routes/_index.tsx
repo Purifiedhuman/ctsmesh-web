50a6ce9f7b4b61e41c0ab51636ad6b2f9f138588
Tey Han Yee <teyhanyee@gmail.com>
2024-06-10 03:23:35 +0800

    feat: initialization

---
+++ app/routes/_index.tsx
@@ -0,0 +1,29 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "~/components/ui/accordion";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </>
+  );
+}
---
+++ remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  appDirectory: "app",
+  ignoredRouteFiles: ["**/*.css"],
+  assetsBuildDirectory: "public/build",
+  tailwind: true,
+  postcss: true,
+};
