616651b29a4d3d0c71fffd34e9c3f69bc906467b
Tey Han Yee <teyhanyee@gmail.com>
2024-10-11 22:01:18 +0800

    feat: handle breakpoint

--- app/(dashboard)/dashboard/facemesh/page.tsx
+++ app/(dashboard)/dashboard/facemesh/page.tsx
@@ -1,20 +1,24 @@
 import BreadCrumb from '@/components/breadcrumb';
 import FaceMesh from '@/components/facemesh/facemesh';
+import { Container } from '@/components/layout/container';
 import { Heading } from '@/components/ui/heading';
 
 const breadcrumbItems = [{ title: 'Cook', link: '/dashboard/facemesh' }];
 export default function page() {
   return (
-    <>
-      <div className="flex-1 space-y-1 md:p-8">
+    <Container>
+      <div className="relative flex-1 space-y-1 md:p-8">
         {/* <BreadCrumb items={breadcrumbItems} /> */}
 
         <div className="flex items-start justify-center p-10 text-center">
-          <Heading title={`Let's Cook!🔥`} description="Swipe to earn! Gas is sponsored!" />
+          <Heading
+            title={`Let's Cook!🔥`}
+            description="Swipe to earn! Gas is sponsored!"
+          />
         </div>
 
         <FaceMesh />
       </div>
-    </>
+    </Container>
   );
 }
--- components/facemesh/facemesh.tsx
+++ components/facemesh/facemesh.tsx
@@ -88,7 +88,8 @@ export default function FaceMesh() {
       finalStatus: 'pending'
     }))
   );
-  const { sendTransactionMutation , account, readTransactionMutation} = useAptosTransaction();
+  const { sendTransactionMutation, account, readTransactionMutation } =
+    useAptosTransaction();
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   // Add this state at the top of your component
   const [rewardAmount, setRewardAmount] = useState<number>(0);
@@ -119,7 +120,9 @@ export default function FaceMesh() {
 
       //     break;
       // }
-      const result = await readTransactionMutation.mutateAsync({ reward_address: account?.address });
+      const result = await readTransactionMutation.mutateAsync({
+        reward_address: account?.address
+      });
       setRewardAmount(Number(result));
       console.log(rewardAmount);
       // Show the popup after successful swipe
@@ -132,8 +135,8 @@ export default function FaceMesh() {
   };
 
   return (
-    <div className="flex flex-col relative">
-      <div className="absolute top-4 right-4 z-10 flex items-center bg-black bg-opacity-50 rounded-full px-3 py-1">
+    <>
+      <div className="absolute right-10 top-[7.3rem] z-10 flex items-center rounded-full bg-black bg-opacity-50 px-3 py-1 md:right-[90px] md:top-36">
         <Image
           src="/static/passion.png"
           alt="Reward"
@@ -141,77 +144,79 @@ export default function FaceMesh() {
           height={24}
           className="mr-2"
         />
-        <span className="text-white font-bold">{rewardAmount}</span>
+        <span className="font-bold text-white">{rewardAmount}</span>
       </div>
-      <div id="facemesh-root">
-        <div className="swiper h-full">
-          <Swiper
-            className="swiper-wrapper"
-            modules={[Autoplay, Navigation, Pagination, EffectTinder]}
-            effect="tinder"
-            grabCursor={true}
-            // @ts-ignore
-            onTinderSwipe={handleSwipe}
-          >
-            {currentKols.map((characterCard) => (
-              <SwiperSlide key={characterCard.id}>
-                <div
-                  style={{
-                    backgroundImage: 'url(' + characterCard.url + ')'
-                  }}
-                  className="relative h-full rounded-lg bg-cover bg-center shadow-lg"
-                >
-                  <h3 className="demo-slide-name">{characterCard.name}</h3>
-                </div>
+      <div className="relative flex flex-col">
+        <div id="facemesh-root">
+          <div className="swiper h-full">
+            <Swiper
+              className="swiper-wrapper"
+              modules={[Autoplay, Navigation, Pagination, EffectTinder]}
+              effect="tinder"
+              grabCursor={true}
+              // @ts-ignore
+              onTinderSwipe={handleSwipe}
+            >
+              {currentKols.map((characterCard) => (
+                <SwiperSlide key={characterCard.id}>
+                  <div
+                    style={{
+                      backgroundImage: 'url(' + characterCard.url + ')'
+                    }}
+                    className="relative h-full rounded-lg bg-cover bg-center shadow-lg"
+                  >
+                    <h3 className="demo-slide-name">{characterCard.name}</h3>
+                  </div>
+                </SwiperSlide>
+              ))}
+
+              <SwiperSlide className="swiper-slide demo-empty-slide">
+                <div>NO MORE RECOMMENDATIONS</div>
               </SwiperSlide>
-            ))}
-
-            <SwiperSlide className="swiper-slide demo-empty-slide">
-              <div>NO MORE RECOMMENDATIONS</div>
-            </SwiperSlide>
-          </Swiper>
+            </Swiper>
+          </div>
         </div>
-      </div>
 
-      <div className="mt-4 flex justify-center gap-4">
-        <Button
-          // onClick={() => {
-          //   handleSwipe(null, 'left');
-          // }}
-          className="swiper-tinder-button swiper-tinder-button-no"
-        >
-          <p className='text-red-500'>Noise</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="48"
-            viewBox="0 -960 960 960"
-            width="48"
+        <div className="mt-4 flex justify-center gap-4">
+          <Button
+            // onClick={() => {
+            //   handleSwipe(null, 'left');
+            // }}
+            className="swiper-tinder-button swiper-tinder-button-no"
           >
-            <path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" />
-          </svg>
-        </Button>
-        <Button
-          // onClick={() => {
-          //   handleSwipe(null, 'right');
-          // }}
-          className="swiper-tinder-button swiper-tinder-button-yes text-green-400"
-        >
-          <p className='text-green-500'>Alpha</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="48"
-            viewBox="0 -960 960 960"
-            width="48"
+            <p className="text-sm md:text-lg">Noise</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="48"
+              viewBox="0 -960 960 960"
+              width="48"
+            >
+              <path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" />
+            </svg>
+          </Button>
+          <Button
+            // onClick={() => {
+            //   handleSwipe(null, 'right');
+            // }}
+            className="swiper-tinder-button swiper-tinder-button-yes text-green-400"
           >
-            <path d="m480-121-41-37q-106-97-175-167.5t-110-126Q113-507 96.5-552T80-643q0-90 60.5-150.5T290-854q57 0 105.5 27t84.5 78q42-54 89-79.5T670-854q89 0 149.5 60.5T880-643q0 46-16.5 91T806-451.5q-41 55.5-110 126T521-158l-41 37Z" />
-          </svg>
-        </Button>
+            <p className="text-sm md:text-lg">Alpha</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="48"
+              viewBox="0 -960 960 960"
+              width="48"
+            >
+              <path d="m480-121-41-37q-106-97-175-167.5t-110-126Q113-507 96.5-552T80-643q0-90 60.5-150.5T290-854q57 0 105.5 27t84.5 78q42-54 89-79.5T670-854q89 0 149.5 60.5T880-643q0 46-16.5 91T806-451.5q-41 55.5-110 126T521-158l-41 37Z" />
+            </svg>
+          </Button>
+        </div>
+        <Popup
+          isOpen={isPopupOpen}
+          onClose={() => setIsPopupOpen(false)}
+          rewardText={`You earned ${rewardAmount} chef's kiss!`}
+        />
       </div>
-      <Popup 
-        isOpen={isPopupOpen} 
-        onClose={() => setIsPopupOpen(false)}
-        rewardText={`You earned ${rewardAmount} chef's kiss!`}
-      />
-    </div>
+    </>
   );
 }
