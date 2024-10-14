f82d69c36739647c78c945d3965d1f14f0310e1b
Ang Norman <norman1997.an@gmail.com>
2024-10-14 14:48:49 +0800

    added readme and deploy alpha

--- app/(dashboard)/dashboard/kanban/page.tsx
+++ app/(dashboard)/dashboard/kanban/page.tsx
@@ -5,7 +5,7 @@ import BreadCrumb from '@/components/breadcrumb'
 import { Heading } from '@/components/ui/heading'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { TweetsInfo } from '@/components/TweetsInfo'
-const breadcrumbItems = [{ title: 'Secret', link: '/dashboard/secret' }]
+const breadcrumbItems = [{ title: 'Alpha', link: '/dashboard/kanban' }]
 
 export default function Page() {
 
@@ -13,7 +13,7 @@ export default function Page() {
     <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
       <BreadCrumb items={breadcrumbItems} />
       <div className="flex items-start justify-between">
-        <Heading title="Secret Cook Recipe" description="Get all the alpha votes" />
+        <Heading title="Alpha Timeline" description="Get all the alpha votes" />
       </div>
       <TweetsInfo />
     </div>
